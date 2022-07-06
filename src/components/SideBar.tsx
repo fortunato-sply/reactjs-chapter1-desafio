f7cbd0e344a234faf9380c5c8abd2e91945cb162
fortunato-sply <lucasfortunato2530@gmail.com>
2022-07-06 11:40:53 -0300

    end of challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "../App";
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onHandleClickButton: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, onHandleClickButton}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onHandleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
